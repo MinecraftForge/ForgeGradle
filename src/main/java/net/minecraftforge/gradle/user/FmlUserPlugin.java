72752b64da9571e6aaa430192ed19f7290cfdba9
Lex Manos <LexManos@gmail.com>
2013-11-06 13:08:08 -0800

    Basic outline.

--- src/main/java/net/minecraftforge/gradle/user/FmlUserPlugin.java
+++ src/main/java/net/minecraftforge/gradle/user/FmlUserPlugin.java
@@ -7,8 +7,37 @@ public class FmlUserPlugin extends UserBasePlugin<FmlUserExtension>
     public void applyPlugin()
     {
         super.applyPlugin();
-        
-        // TODO tasks....
+    /*
+        setupDevWorkspace
+          Downloads:
+            notch->srg Srg File
+            Exceptor info
+            merge config
+            FML/Forge src file
+            Decomp:
+              Mapping Info, with full comments
+              MCP patches
+              FML/Forge patches
+            No Decomp:
+              Mapping Info (fields/methods/params.csv minus comments?)
+              Binpatches
+              Javadoc jar
+                
+          Process:
+            Download MC/Server
+            Merged
+            No Decomp:
+              Apply BinPatches
+              Apply mapping to notch->srg to get notch->mapped
+              Apply notch->mapped to merged
+              Resulting jar is 'minecraft.jar'
+              Link Javadocs to ',inecraft.jar'
+              Link jar containing FML/Forge's src files to 'minecraft.jar'
+            Decomp:
+              Full MCP decompile process
+                 should result in: minecraft.jar {recompiled version fo the fully mapped source with commends and the like} 
+                 minecraft-source.jar a jar of the decompiled code, the source shoukd bnever be linked in the final workspace
+     */
     }
     
     protected Class<FmlUserExtension> getExtensionClass(){ return FmlUserExtension.class; }
