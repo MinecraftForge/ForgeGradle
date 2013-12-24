9d995b6841e3134f6fc8cdc527e0e79b3288bd50
Lex Manos <LexManos@gmail.com>
2013-12-23 21:08:46 -0800

    Don't makr replace javadoc output as a output so gradle doesn't barf.

--- src/main/java/net/minecraftforge/gradle/tasks/ReplaceJavadocsTask.java
+++ src/main/java/net/minecraftforge/gradle/tasks/ReplaceJavadocsTask.java
@@ -27,7 +27,7 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileTree;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputFile;
+//import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 import au.com.bytecode.opencsv.CSVReader;
@@ -43,7 +43,7 @@ public class ReplaceJavadocsTask extends DefaultTask
     @InputFiles
     private LinkedList<DelayedFile>                     inFiles = new LinkedList<DelayedFile>();
 
-    @OutputFile
+    //@OutputFile
     private DelayedFile                            outFile;
 
     @InputFile
