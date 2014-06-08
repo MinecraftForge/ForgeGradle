57d300550677b539677a940ebfed99e790f259aa
jk-5 <jk-5@users.noreply.github.com>
2014-06-08 08:09:56 +0200

    Don't create a .gitignore directory. Create the parent

--- src/main/java/net/minecraftforge/gradle/user/UserBasePlugin.java
+++ src/main/java/net/minecraftforge/gradle/user/UserBasePlugin.java
@@ -1109,7 +1109,7 @@ public abstract class UserBasePlugin<T extends UserExtension> extends BasePlugin
         File git = new File(project.getBuildDir(), ".gitignore");
         if (!git.exists())
         {
-            git.mkdir();
+            git.getParentFile().mkdir();
             try
             {
                 Files.write("#Seriously guys, stop commiting this to your git repo!\r\n*".getBytes(), git);
