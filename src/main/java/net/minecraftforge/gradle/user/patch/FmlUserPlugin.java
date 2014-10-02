1136585ff3c57094ecf62b0474c70f2245c77c74
AbrarSyed <sacabrarsyed@gmail.com>
2014-10-01 19:05:19 -0500

    fixed double shaes in FML

--- src/main/java/net/minecraftforge/gradle/user/patch/FmlUserPlugin.java
+++ src/main/java/net/minecraftforge/gradle/user/patch/FmlUserPlugin.java
@@ -42,7 +42,7 @@ public class FmlUserPlugin extends UserPatchBasePlugin
     @Override
     public String resolve(String pattern, Project project, UserPatchExtension exten)
     {
-        pattern = pattern.replace("{API_GROUP}", getMcVersion(exten).startsWith("1.8") ? "net.minecraftforge." : "cpw.mods.");
+        pattern = pattern.replace("{API_GROUP}", getMcVersion(exten).startsWith("1.8") ? "net.minecraftforge" : "cpw.mods");
         
         pattern = super.resolve(pattern, project, exten);
         
