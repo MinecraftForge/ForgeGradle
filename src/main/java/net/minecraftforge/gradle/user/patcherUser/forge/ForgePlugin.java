0a701c82b2c1ed5006fa4d757833468e5c2a371f
AbrarSyed <sacabrarsyed@gmail.com>
2015-08-26 14:20:58 -0500

    added location injetion to forge plugin UNTESTED

--- src/main/java/net/minecraftforge/gradle/user/patcherUser/forge/ForgePlugin.java
+++ src/main/java/net/minecraftforge/gradle/user/patcherUser/forge/ForgePlugin.java
@@ -5,7 +5,15 @@ import static net.minecraftforge.gradle.common.Constants.SRG_MCP_TO_SRG;
 import static net.minecraftforge.gradle.user.UserConstants.TASK_REOBF;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import org.gradle.api.Action;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.bundling.Jar;
+
+import com.google.common.base.Strings;
 
 import net.minecraftforge.gradle.common.Constants;
 import net.minecraftforge.gradle.user.TaskSingleReobf;
@@ -17,13 +25,9 @@ import net.minecraftforge.gradle.util.json.fgversion.FGVersion;
 import net.minecraftforge.gradle.util.json.fgversion.FGVersionWrapper;
 import net.minecraftforge.gradle.util.json.forgeversion.ForgeVersion;
 
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.bundling.Jar;
-
-import com.google.common.base.Strings;
-
 public class ForgePlugin extends PatcherUserBasePlugin<ForgeExtension>
 {
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     protected void applyUserPlugin()
     {
@@ -38,6 +42,51 @@ public class ForgePlugin extends PatcherUserBasePlugin<ForgeExtension>
             reobf.setPrimarySrg(delayedFile(SRG_MCP_TO_SRG));
             reobf.addPreTransformer(new McVersionTransformer(delayedString(REPLACE_MC_VERSION)));
         }
+        
+        // configure eclipse task to do extra stuff.
+        project.getTasks().getByName("eclipse").doLast(new Action() {
+
+            @Override
+            public void execute(Object arg0)
+            {
+                // find the file
+                File f = new File("eclipse/.metadata/.plugins/org.eclipse.core.resources/.projects");
+
+                if (!f.exists()) // folder doesnt exist
+                {
+                    return;
+                }
+
+                File[] files = f.listFiles();
+                if (files.length < 1) // empty folder
+                    return;
+
+                f = new File(files[0], ".location");
+
+                if (f.exists()) // if .location exists
+                {
+                    String projectDir = "URI//" + project.getProjectDir().toURI().toString();
+                    try
+                    {
+                        byte[] LOCATION_BEFORE = new byte[] { 0x40, (byte) 0xB1, (byte) 0x8B, (byte) 0x81, 0x23, (byte) 0xBC, 0x00, 0x14, 0x1A, 0x25, (byte) 0x96, (byte) 0xE7, (byte) 0xA3, (byte) 0x93, (byte) 0xBE, 0x1E };
+                        byte[] LOCATION_AFTER = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xC0, 0x58, (byte) 0xFB, (byte) 0xF3, 0x23, (byte) 0xBC, 0x00, 0x14, 0x1A, 0x51, (byte) 0xF3, (byte) 0x8C, 0x7B, (byte) 0xBB, 0x77, (byte) 0xC6 };
+
+                        FileOutputStream fos = new FileOutputStream(f);
+                        fos.write(LOCATION_BEFORE);//Unknown but w/e
+                        fos.write((byte) ((projectDir.length() & 0xFF) >> 8));
+                        fos.write((byte) ((projectDir.length() & 0xFF) >> 0));
+                        fos.write(projectDir.getBytes());
+                        fos.write(LOCATION_AFTER);//Unknown but w/e
+                        fos.close();
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        });
     }
 
     @Override
