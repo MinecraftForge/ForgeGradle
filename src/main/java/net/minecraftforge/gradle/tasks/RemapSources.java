0db4c8796336fac9b58d7cc90849744bd9631075
AbrarSyed <sacabrarsyed@gmail.com>
2015-11-24 22:43:28 -0600

    stopped remapping names like p_110579_2_f with the trailing f there

--- src/main/java/net/minecraftforge/gradle/tasks/RemapSources.java
+++ src/main/java/net/minecraftforge/gradle/tasks/RemapSources.java
@@ -59,7 +59,7 @@ public class RemapSources extends AbstractEditJarTask
     private final Map<String, String> fieldDocs    = Maps.newHashMap();
     private final Map<String, String> params       = Maps.newHashMap();
 
-    private static final Pattern      SRG_FINDER   = Pattern.compile("func_[0-9]+_[a-zA-Z_]+|field_[0-9]+_[a-zA-Z_]+|p_[\\w]+_\\d+_");
+    private static final Pattern      SRG_FINDER   = Pattern.compile("func_[0-9]+_[a-zA-Z_]+|field_[0-9]+_[a-zA-Z_]+|p_[\\w]+_\\d+_\\b");
     private static final Pattern      METHOD       = Pattern.compile("^((?: {4})+|\\t+)(?:[\\w$.\\[\\]]+ )+(func_[0-9]+_[a-zA-Z_]+)\\(");
     private static final Pattern      FIELD        = Pattern.compile("^((?: {4})+|\\t+)(?:[\\w$.\\[\\]]+ )+(field_[0-9]+_[a-zA-Z_]+) *(?:=|;)");
 
