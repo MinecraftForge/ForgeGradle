57aad122cc4b02cd6a8c660cf11be5cc29edead4
LexManos <LexManos@gmail.com>
2019-02-06 19:29:20 -0800

    Include MCP Mappings zip in classpath of patcher dev time.

--- src/patcher/java/net/minecraftforge/gradle/patcher/PatcherPlugin.java
+++ src/patcher/java/net/minecraftforge/gradle/patcher/PatcherPlugin.java
@@ -417,6 +417,7 @@ public class PatcherPlugin implements Plugin<Project> {
             }
             project.getDependencies().add(MC_DEP_CONFIG, "net.minecraft:client:" + extension.mcVersion + ":extra");
             project.getDependencies().add(MC_DEP_CONFIG, "net.minecraft:client:" + extension.mcVersion + ":data");
+            project.getDependencies().add(MC_DEP_CONFIG, extension.getMappings());
 
             if (dlMCMetaConfig.get().getMCVersion() == null) {
                 dlMCMetaConfig.get().setMCVersion(extension.mcVersion);
