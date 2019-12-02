a34d73dec87d8bc1174d40cc2eeea11a7db11717
LexManos <LexManos@gmail.com>
2019-12-02 15:58:59 -0800

    Bump Access Transformer default version.

--- src/common/java/net/minecraftforge/gradle/common/util/Utils.java
+++ src/common/java/net/minecraftforge/gradle/common/util/Utils.java
@@ -83,7 +83,7 @@ public class Utils {
                                                           //manifest doesn't include sha1's so we use this for the per-version json as well.
     public static final String FORGE_MAVEN = "https://files.minecraftforge.net/maven/";
     public static final String BINPATCHER =  "net.minecraftforge:binarypatcher:1.+:fatjar";
-    public static final String ACCESSTRANSFORMER = "net.minecraftforge:accesstransformers:0.14.+:fatjar";
+    public static final String ACCESSTRANSFORMER = "net.minecraftforge:accesstransformers:1.0.+:fatjar";
     public static final String SPECIALSOURCE = "net.md-5:SpecialSource:1.8.3:shaded";
     public static final String SRG2SOURCE =  "net.minecraftforge:Srg2Source:5.+:fatjar";
     public static final String SIDESTRIPPER = "net.minecraftforge:mergetool:1.0.7:fatjar";
