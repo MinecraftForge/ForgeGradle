967617cacf40c80e17596fbeb95874dcf45cc76d
Unnoen <unnoen@users.noreply.github.com>
2020-06-18 13:04:53 +1000

    Use VersionJson class for current OS.

--- src/common/java/net/minecraftforge/gradle/common/util/Utils.java
+++ src/common/java/net/minecraftforge/gradle/common/util/Utils.java
@@ -637,12 +637,14 @@ public class Utils {
     public static File getMCDir()
     {
         String userHomeDir = System.getProperty("user.home", ".");
-        String osType = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
         String mcDir = ".minecraft";
-        if (osType.contains("win") && System.getenv("APPDATA") != null)
-            return new File(System.getenv("APPDATA"), mcDir);
-        else if (osType.contains("mac"))
-            return new File(new File(new File(userHomeDir, "Library"),"Application Support"),"minecraft");
-        return new File(userHomeDir, mcDir);
+        File directory = new File(userHomeDir, mcDir);
+        switch (VersionJson.OS.getCurrent()) {
+            case WINDOWS: if (System.getenv("APPDATA") != null) directory = new File(System.getenv("APPDATA"), mcDir);
+            break;
+            case OSX: directory = new File(new File(new File(userHomeDir, "Library"),"Application Support"),"minecraft");
+            break;
+        }
+        return directory;
     }
 }
