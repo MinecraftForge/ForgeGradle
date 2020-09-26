5205390ac812c2de9a108ed731ab356299e3bae9
Gabriel Harris-Rouquette <gabizou@me.com>
2020-09-25 23:20:07 -0700

    Update HackyJavaCompile to create Gradle6 clean compiler
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/userdev/java/net/minecraftforge/gradle/userdev/tasks/HackyJavaCompile.java
+++ src/userdev/java/net/minecraftforge/gradle/userdev/tasks/HackyJavaCompile.java
@@ -21,18 +21,27 @@
 package net.minecraftforge.gradle.userdev.tasks;
 
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.internal.TaskOutputsInternal;
 import org.gradle.api.internal.tasks.compile.CleaningJavaCompiler;
 import org.gradle.api.internal.tasks.compile.DefaultJavaCompileSpec;
 import org.gradle.api.internal.tasks.compile.JavaCompileSpec;
 import org.gradle.api.tasks.WorkResult;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.internal.nativeintegration.filesystem.FileSystem;
+import org.gradle.internal.os.OperatingSystem;
+import org.gradle.internal.time.Clock;
+import org.gradle.internal.time.Time;
 import org.gradle.jvm.internal.toolchain.JavaToolChainInternal;
 import org.gradle.language.base.internal.compile.Compiler;
 import org.gradle.language.base.internal.compile.CompilerUtil;
 import org.gradle.util.GradleVersion;
 
+import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.function.LongSupplier;
+import java.util.function.Predicate;
 
 // A terrible hack to use JavaCompile while bypassing
 // Gradle's normal task infrastructure.
@@ -86,7 +95,30 @@ public class HackyJavaCompile extends JavaCompile {
                 }
                 spec.setSourceFiles(getSource());
                 Compiler<JavaCompileSpec> javaCompiler = CompilerUtil.castCompiler(((JavaToolChainInternal) getToolChain()).select(getPlatform()).newCompiler(spec.getClass()));
-                CleaningJavaCompiler compiler = new CleaningJavaCompiler(javaCompiler, getOutputs());
+                final CleaningJavaCompiler compiler;
+                if (GradleVersion.current().compareTo(GradleVersion.version("6.0.0")) >= 0) {
+                    // Welcome to Gradle, we sell Hacks and Hack Accessories
+                    final Class<?> deleterInterface = Class.forName("org.gradle.internal.file.Deleter");
+                    final Constructor<CleaningJavaCompiler> constructor = CleaningJavaCompiler.class.getConstructor(Compiler.class, TaskOutputsInternal.class, deleterInterface);
+                    final Class<?> defaultDeleterClazz = Class.forName("org.gradle.internal.file.impl.DefaultDeleter");
+                    final Constructor<?> defaultDeleterCtor = defaultDeleterClazz.getConstructor(LongSupplier.class,
+                        Predicate.class, Boolean.TYPE);
+                    final Clock clock = Time.clock();
+                    final LongSupplier timeProvider = clock::getCurrentTime;
+                    final FileSystem fileSystem;
+                    try {
+                        final Class<?> fileSystemClass = Class.forName("org.gradle.internal.nativeintegration.services.FileSystems");
+                        final Method getDefaultFileSystem = fileSystemClass.getMethod("getDefault");
+                        fileSystem = (FileSystem) getDefaultFileSystem.invoke(null);
+                    } catch (IllegalAccessException | IllegalArgumentException | Error | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                        throw new UnsupportedOperationException("Could not get the Gradle FileSystem object", e);
+                    }
+                    final Predicate<? super File> isSymLink = fileSystem::isSymlink;
+                    final Object deleter = defaultDeleterCtor.newInstance(timeProvider, isSymLink, OperatingSystem.current().isWindows());
+                    compiler = constructor.newInstance(javaCompiler, getOutputs(), deleter);
+                } else {
+                    compiler = new CleaningJavaCompiler(javaCompiler, getOutputs());
+                }
                 final WorkResult execute = compiler.execute(spec);
                 setDidWork(execute.getDidWork());
             } catch (Exception e) {
