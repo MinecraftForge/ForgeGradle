3f40205e9e72635521bceb2852a5d4f4651bea9e
shedaniel <daniel@shedaniel.me>
2021-01-30 16:26:18 +0800

    Use Project#javaexec instead of creating a JavaExec task (#744)
    * Use Project#javaexec instead of creating a JavaExec task
    
    * Don't close the logger there
    
    * Forgot to throw here

--- src/common/java/net/minecraftforge/gradle/common/task/JarExec.java
+++ src/common/java/net/minecraftforge/gradle/common/task/JarExec.java
@@ -37,7 +37,6 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
@@ -69,37 +68,35 @@ public class JarExec extends DefaultTask {
 
         File logFile = new File(workDir, "log.txt");
 
-        JavaExec java = getProject().getTasks().create("_java_exec_" + index++ + "_", JavaExec.class);
         try (OutputStream log = hasLog ? new BufferedOutputStream(new FileOutputStream(logFile)) : NULL) {
             PrintWriter printer = new PrintWriter(log, true);
-            // Execute command
-            java.setArgs(filterArgs());
-            printer.println("Args: " + java.getArgs().stream().map(m -> '"' + m +'"').collect(Collectors.joining(", ")));
-            if (getClasspath() == null)
-                java.setClasspath(getProject().files(jar));
-            else
-                java.setClasspath(getProject().files(jar, getClasspath()));
-            java.getClasspath().forEach(f -> printer.println("Classpath: " + f.getAbsolutePath()));
-            java.setWorkingDir(workDir);
-            printer.println("WorkDir: " + workDir);
-            java.setMain(mainClass);
-            printer.println("Main: " + mainClass);
-            printer.println("====================================");
-            java.setStandardOutput(new OutputStream() {
-                @Override
-                public void flush() throws IOException {
-                    log.flush();
-                }
-                @Override
-                public void close() {}
-                @Override
-                public void write(int b) throws IOException {
-                    log.write(b);
-                }
-            });
-            java.exec();
-        } finally {
-            java.setEnabled(false);
+            getProject().javaexec(java -> {
+                // Execute command
+                java.setArgs(filterArgs());
+                printer.println("Args: " + java.getArgs().stream().map(m -> '"' + m +'"').collect(Collectors.joining(", ")));
+                if (getClasspath() == null)
+                    java.setClasspath(getProject().files(jar));
+                else
+                    java.setClasspath(getProject().files(jar, getClasspath()));
+                java.getClasspath().forEach(f -> printer.println("Classpath: " + f.getAbsolutePath()));
+                java.setWorkingDir(workDir);
+                printer.println("WorkDir: " + workDir);
+                java.setMain(mainClass);
+                printer.println("Main: " + mainClass);
+                printer.println("====================================");
+                java.setStandardOutput(new OutputStream() {
+                    @Override
+                    public void flush() throws IOException {
+                        log.flush();
+                    }
+                    @Override
+                    public void close() {}
+                    @Override
+                    public void write(int b) throws IOException {
+                        log.write(b);
+                    }
+                });
+            }).rethrowFailure().assertNormalExitValue();
         }
 
         if (hasLog)
