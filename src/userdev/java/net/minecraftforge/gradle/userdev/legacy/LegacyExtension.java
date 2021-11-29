2b2b815a71832a058b64403a38dc4f1a84d7327e
Curle <curle@gemwire.uk>
2021-11-29 21:32:51 +0000

    Implement start of the Legacy Extension and Retrogradle fixes

--- src/userdev/java/net/minecraftforge/gradle/userdev/legacy/LegacyExtension.java
+++ src/userdev/java/net/minecraftforge/gradle/userdev/legacy/LegacyExtension.java
@@ -4,7 +4,6 @@ import groovy.lang.GroovyObjectSupport;
 import net.minecraftforge.gradle.userdev.UserDevPlugin;
 import net.minecraftforge.srgutils.MinecraftVersion;
 import org.gradle.api.Project;
-import org.gradle.api.provider.Property;
 
 
 /**
@@ -17,30 +16,36 @@ import org.gradle.api.provider.Property;
  *
  * It allows for configuring RetroGradle patches and fixes, which are otherwise set by version.
  * Each fix is documented in this class and in the application function.
- * {@link UserDevPlugin#runRetrogradleFixes}
+ * {@link UserDevPlugin#runRetrogradleFixes()}
  *
  * @author Curle
  */
-public abstract class LegacyExtension extends GroovyObjectSupport {
+public class LegacyExtension extends GroovyObjectSupport {
     public static final String EXTENSION_NAME = "legacy";
-    private static final MinecraftVersion FG3 = MinecraftVersion.from("1.13");
 
     public LegacyExtension(Project project) {
-        getFixClasspath().convention(project.provider(() -> {
-            final MinecraftVersion version = MinecraftVersion.from((String) project.getExtensions().getExtraProperties().get("MC_VERSION"));
-            
-            // fixClasspath by default if version is below FG 3
-            return version.compareTo(FG3) < 0;
-        }));
+        final MinecraftVersion version = MinecraftVersion.from((String) project.getExtensions().getExtraProperties().get("MC_VER"));
+        final MinecraftVersion FG2_3 = MinecraftVersion.from("1.12.2");
+
+        // fixClasspath by default if version is below FG 2.3
+        fixClasspath = version.compareTo(FG2_3) < 0;
     }
 
     /**
      * fixClassPath;
      *  FG2 Userdev puts all classes and resources into a single jar file for FML to consume.
      *  FG3+ puts classes and resources into separate folders, which breaks on older versions.
-     *  We replicate the FG2 behavior by replacing these folders by the jar artifact on the runtime classpath.
+     *  We replicate the FG2 behavior by forcing the classes and resources to go to the same build folder.
      *
      * Takes a boolean - true for apply fix, false for no fix.
      */
-    public abstract Property<Boolean> getFixClasspath();
+    private boolean fixClasspath;
+
+    // fixClasspath Getter and buildscript integration.
+    public boolean getFixClasspath() { return fixClasspath; }
+
+    // fixClasspath Setter
+    public void setFixClasspath(boolean shouldFix) {
+        fixClasspath = shouldFix;
+    }
 }
