be5dfca7ab55b114ecf327c4f77f3238b0d0eab2
Marc Hermans <marc.hermans@ldtteam.com>
2022-02-24 17:27:59 +0100

    Build all FG_* branches as the main branch build.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,6 +35,9 @@ project {
         text("github_repository_name", "ForgeGradle", label = "The github repository name. Used to connect to it in VCS Roots.", description = "This is the repository slug on github. So for example `ForgeGradle` or `MinecraftForge`. It is interpolated into the global VCS Roots.", display = ParameterDisplay.HIDDEN, allowEmpty = false)
         text("env.PUBLISHED_JAVA_ARTIFACT_ID", "ForgeGradle", label = "Published artifact id", description = "The maven coordinate artifact id that has been published by this build. Can not be empty.", allowEmpty = false)
         text("env.PUBLISHED_JAVA_GROUP", "net.minecraftforge.gradle", label = "Published group", description = "The maven coordinate group that has been published by this build. Can not be empty.", allowEmpty = false)
+        text("git_branch_spec", """
+                +:refs/heads/(FG_*)
+            """.trimIndent(), label = "The branch specification of the repository", description = "By default all main branches are build by the configuration. Modify this value to adapt the branches build.", display = ParameterDisplay.HIDDEN, allowEmpty = true)
     }
 
     features {
