e9ef2d31c66a96932efaab2788f63549feca1783
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-06-16 22:21:13 -0700

    Fixed SlimeLauncherExec

--- src/main/groovy/net/minecraftforge/gradle/SlimeLauncherExec.groovy
+++ src/main/groovy/net/minecraftforge/gradle/SlimeLauncherExec.groovy
@@ -54,7 +54,7 @@ import java.nio.file.Files
             task.inherit(configs, options.name)
             options.apply(task)
 
-            task.classpath task.getTool(Tools.SLIME_LAUNCHER)
+            task.classpath task.getTool(Tools.SLIME_LAUNCHER, getProject())
 
             if (task.buildAllProjects)
                 task.dependsOn project.allprojects.collect { it.tasks.named(LifecycleBasePlugin.ASSEMBLE_TASK_NAME) }.toArray()
