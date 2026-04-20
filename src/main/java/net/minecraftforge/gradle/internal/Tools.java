8c386f54c18a7dbf3443cef72fdad257ac05188b
LexManos <LexManos@gmail.com>
2026-04-20 16:22:46 -0700

    Bump SlimeLauncher, better reflection for older versions Bump Mavenizer, better supports 1.12.2 code formating, and scala dependencies

--- src/main/java/net/minecraftforge/gradle/internal/Tools.java
+++ src/main/java/net/minecraftforge/gradle/internal/Tools.java
@@ -9,7 +9,7 @@ import net.minecraftforge.gradleutils.shared.Tool;
 final class Tools {
     private Tools() { }
 
-    static final Tool SLIMELAUNCHER = Tool.ofForge("slimelauncher", "net.minecraftforge:slime-launcher:0.1.12", 8, "net.minecraftforge.launcher.Main");
+    static final Tool SLIMELAUNCHER = Tool.ofForge("slimelauncher", "net.minecraftforge:slime-launcher:0.1.14", 8, "net.minecraftforge.launcher.Main");
 
-    static final Tool MAVENIZER = Tool.ofForge("mavenizer", "net.minecraftforge:minecraft-mavenizer:0.4.61", 25, "net.minecraftforge.mcmaven.cli.Main");
+    static final Tool MAVENIZER = Tool.ofForge("mavenizer", "net.minecraftforge:minecraft-mavenizer:0.4.65", 25, "net.minecraftforge.mcmaven.cli.Main");
 }
